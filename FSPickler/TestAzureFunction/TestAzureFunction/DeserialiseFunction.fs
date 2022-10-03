ebd3d9612afdb0dc59ed213b4d54b1302a894e92
Christopher.Dunderdale <Christopher.Dunderdale@dyna-mo.com>
2022-10-03 13:42:01 +0200

    FSPickle example

---
+++ FSPickler/TestAzureFunction/TestAzureFunction/DeserialiseFunction.fs
@@ -0,0 +1,34 @@
+﻿namespace TestAzureFunction
+
+open System
+open System.IO
+open System.Net.Http
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open Newtonsoft.Json
+open Microsoft.Extensions.Logging
+open MBrace.FsPickler
+
+module DeserialiseFunction =
+
+    [<FunctionName("DeserialiseFunction")>]
+    let run ([<HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)>] req: HttpRequestMessage) (log: ILogger) =
+        async {
+            
+            let binarySerializer = FsPickler.CreateBinarySerializer()
+
+            let unpickle =
+                req.Content.ReadAsByteArrayAsync()
+                |> Async.AwaitTask
+                |> Async.RunSynchronously
+
+            let test =
+                unpickle
+                |> binarySerializer.UnPickle<double->double>
+
+            let testResult = test 2.
+            return OkObjectResult() :> IActionResult
+            
+        } |> Async.StartAsTask
---
+++ FSPickler/TestAzureFunction/UnitTest/Tests.fs
@@ -0,0 +1,46 @@
+﻿namespace UnitTest
+
+open System
+
+
+module Tests =
+
+    open System.Threading
+    
+    open MBrace.FsPickler
+    open Xunit
+    open System.Net.Http    
+    [<Fact(DisplayName = "DeserialisationTest")>]
+    let deserialiseTest () =
+        let testFunction = fun (input : double) -> input * 2.
+
+        let url = "http://localhost:7071/api/DeserialiseFunction"
+
+        let binarySerializer = FsPickler.CreateBinarySerializer()
+
+        let pickle =
+            testFunction
+            |> binarySerializer.Pickle<double -> double>
+        
+        let binarySerializer2 = FsPickler.CreateBinarySerializer()
+        
+        /// Validation that unpickling locally works fine
+        let testUnpickle : double -> double =
+            pickle
+            |> binarySerializer2.UnPickle<double -> double>
+        
+        let test = testUnpickle 2.
+        
+        
+        let postMethod = HttpMethod("POST")
+        let request = new HttpRequestMessage(postMethod, Uri(url))
+        let client = new HttpClient()
+        let content = new ByteArrayContent(pickle)
+        request.Content <- content
+        
+        let response =
+            client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, CancellationToken())
+            |> Async.AwaitTask
+            |> Async.RunSynchronously
+
+        Assert.True true
